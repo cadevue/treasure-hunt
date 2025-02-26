7822dcf99982845b0c6c1a91204f9c67d7761d3b
Addin Munawwar <moonawar@protonmail.com>
2025-02-26 18:14:51 +0700

    feat: visualizer options input

--- src/sections/HeaderSection.tsx
+++ src/sections/HeaderSection.tsx
@@ -2,7 +2,7 @@ const HeaderSection = () => {
     return (
         <header className="w-full flex flex-col items-center justify-center gap-2 sm:gap-4 text-center">
             <h1 className="text-main-red">
-                Treasure Hunt Solver
+                Treasure Hunt Solver&nbsp;🧭
             </h1>
             <p className="text-base sm:text-xl">Search a Treasure in a maze using <b>BFS/DFS</b></p>
         </header>
