9c93e9172ac6a58b5ffb1a9b1c4f125fb883a25d
Addin Munawwar <moonawar@protonmail.com>
2025-02-28 10:21:24 +0700

    style: change bg

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
     return (
         <>
-        <div className={`inset-0 bg-[url('assets/bg.jpg')] bg-cover bg-center opacity-5 fixed`}/>
+        <div className={`inset-0 bg-[url('assets/bg2.jpg')] bg-cover bg-center opacity-[.075] fixed`}/>
         <ContentLayout>
             <div className="flex flex-col items-center gap-10 py-10">
                 <HeaderSection />
